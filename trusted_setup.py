9d746ef7c2db557ee0ca91377a8801190cec7b05
Hunter Lindauer <holindauer@gmail.com>
2024-06-25 18:07:36 -0700

    fixed bug regarding encrypted t(tau) * powers of tau. Was resulting in a single eliptic curve point instead of 3 due to use of eliptic dot in the wrong area

--- trusted_setup.py
+++ trusted_setup.py
@@ -15,36 +15,37 @@ class TrustedSetup:
         GF = galois.GF(79)
         field_modulus = 79
     
-        # create t = (x - 1)(x - 2) * ... * (x - n)
-        x_min_i = [galois.Poly([1, field_modulus - i], field = GF) for i in range(1, degree)]
+        # create t = (x - 1)(x - 2)(x - 3)(x - 4)
+        x_min_i = [galois.Poly([1, field_modulus - i], field = GF) for i in range(1, 5)]
         t = reduce(lambda x, y: x * y, x_min_i)
 
         # set random tau
-        tau = secret_tau(field_modulus)
+        tau = secret_tau(GF, field_modulus)
 
         # generate G1 and G2 powers for polynomial evaluation
         G1_powers, G2_powers = tau_powers(tau, degree)
 
         # eval t at G1 powers
-        t_G1 = t_eval_G1_powers(t, G1_powers)
+        t_G1 = t_eval_G1_powers(t, tau, 3, G1_powers)
 
             
         return G1_powers, G2_powers, t_G1
 
 
-def secret_tau(field_modulus):
-    return random.randint(1, field_modulus)
+def secret_tau(GF, field_modulus):
+    return GF(random.randint(1, field_modulus))
 
 def tau_powers( tau, poly_degree):
-    G1_powers = [multiply(G1, tau**i) for i in range(poly_degree)]
-    G2_powers = [multiply(G2, tau**i) for i in range(poly_degree)]
+    G1_powers = [multiply(G1, int(tau**i)) for i in range(poly_degree)]
+    G2_powers = [multiply(G2, int(tau**i)) for i in range(poly_degree)]
     return G1_powers, G2_powers
 
 def encrypted_dot(ec_pts, coeffs): 
     return reduce(add, (multiply(pt, int(c)) for pt, c in zip(ec_pts, coeffs)), Z1)
 
-def t_eval_G1_powers(t, G1_powers):
-    return encrypted_dot(G1_powers, t.coeffs)
+def t_eval_G1_powers(t, tau, poly_degree):
+    # encrypt pre evaluated t at powers of tau
+    return [multiply(G1, int(t(tau) * (tau**i))) for i in range(poly_degree)]
         
 
 
